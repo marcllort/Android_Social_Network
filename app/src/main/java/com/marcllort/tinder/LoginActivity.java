7422433740bb03c9406f0edd772c28312618a165
mac12llm <mac12llm@gmail.com>
2019-04-22 11:32:08 +0200

    Added login, register and main basic logic

--- app/src/main/java/com/marcllort/tinder/LoginActivity.java
+++ app/src/main/java/com/marcllort/tinder/LoginActivity.java
@@ -1,10 +1,19 @@
 package com.marcllort.tinder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.support.design.widget.TextInputEditText;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+
+    private TextInputEditText textMail;
+    private TextInputEditText textPassword;
+    private Button btnLogin;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +21,33 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        textMail = findViewById(R.id.email_input_edit);
+        textPassword = findViewById(R.id.password_input_edit);
+
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Click login! Camp login: " + textMail.getText().toString(), Toast.LENGTH_SHORT).show();
+                // Si login correcte, anem a main activity
+                Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        });
+
+
+        btnRegister = findViewById(R.id.btn_signup);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Click Register! Camp password: " + textPassword.getText().toString(), Toast.LENGTH_SHORT).show();
+
+                Intent registerIntent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
 
     }
 
--- app/src/main/java/com/marcllort/tinder/RegisterActivity.java
+++ app/src/main/java/com/marcllort/tinder/RegisterActivity.java
@@ -2,8 +2,19 @@ package com.marcllort.tinder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends Activity {
+
+    private TextInputEditText textMail;
+    private TextInputEditText textPassword;
+    private TextInputEditText textRePassword;
+    private Button btnRegister;
+    private TextView txtLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +22,27 @@ public class RegisterActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        textMail = findViewById(R.id.email_input_edit);
+        textPassword = findViewById(R.id.password_input_edit);
+        textRePassword = findViewById(R.id.repassword_input_edit);
+
+        btnRegister = findViewById(R.id.btn_signup);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(RegisterActivity.this, "Click Register! Camp password: " + textPassword.getText().toString() + " Camp rePassword: " + textRePassword.getText().toString(), Toast.LENGTH_SHORT).show();
+                // Si es registra correctament, anem a la pantalla de login
+                finish();
+            }
+        });
+
+        txtLogin = findViewById(R.id.text_login);
+        txtLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 
