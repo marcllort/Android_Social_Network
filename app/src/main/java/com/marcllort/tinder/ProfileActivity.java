2e7fe4384d9bcdcae220df08cdcaef1a059a9235
mac12llm <mac12llm@gmail.com>
2019-04-26 19:53:26 +0200

    #5 Profile screen fixes

--- app/src/main/java/com/marcllort/tinder/ProfileActivity.java
+++ app/src/main/java/com/marcllort/tinder/ProfileActivity.java
@@ -1,9 +1,20 @@
 package com.marcllort.tinder;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class ProfileActivity extends Activity {
+
+    private EditText dateText;
+    private Calendar myCalendar = Calendar.getInstance(Locale.US);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +22,39 @@ public class ProfileActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        dateSelector();
+
+
+    }
+
+    private void dateSelector() {
+        dateText = (EditText) findViewById(R.id.birth);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                String myFormat = "yyyy-MM-dd"; //In which you need put here
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.FRANCE);
+
+                dateText.setText(sdf.format(myCalendar.getTime()));
+            }
+
+        };
+
+        dateText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(ProfileActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
     }
 
