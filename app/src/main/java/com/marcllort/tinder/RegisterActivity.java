3b9bb7948d516ff9f1a26140256a5beaecf893f3
Marcel <marcel.llopis@hotmail.com>
2019-05-12 18:35:12 +0200

    Function validate RegisterActivity

--- app/src/main/java/com/marcllort/tinder/RegisterActivity.java
+++ app/src/main/java/com/marcllort/tinder/RegisterActivity.java
@@ -1,18 +1,23 @@
 package com.marcllort.tinder;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.marcllort.tinder.API.RegisterCallBack;
+import com.marcllort.tinder.API.RestAPIManager;
 
 public class RegisterActivity extends Activity implements RegisterCallBack {
 
+    private TextInputEditText textUser;
     private TextInputEditText textMail;
     private TextInputEditText textPassword;
     private TextInputEditText textRePassword;
@@ -25,6 +30,7 @@ public class RegisterActivity extends Activity implements RegisterCallBack {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        textUser = findViewById(R.id.user_input_edit);
         textMail = findViewById(R.id.email_input_edit);
         textPassword = findViewById(R.id.password_input_edit);
         textRePassword = findViewById(R.id.repassword_input_edit);
@@ -35,11 +41,20 @@ public class RegisterActivity extends Activity implements RegisterCallBack {
             public void onClick(View v) {
                 Toast.makeText(RegisterActivity.this, "Click Register! Camp password: " + textPassword.getText().toString() + " Camp rePassword: " + textRePassword.getText().toString(), Toast.LENGTH_SHORT).show();
                 // Si es registra correctament, anem a la pantalla de login
-                finish();
+                if (validate()) {
+                    String username = textUser.getText().toString();
+                    String email = textMail.getText().toString();
+                    String password = textPassword.getText().toString();
+                    String repassword = textRePassword.getText().toString();
 
-                Intent profileIntent = new Intent(getApplicationContext(), ProfileCreateActivity.class);
-                startActivity(profileIntent);
+                    // Implemetan el register AQUI
+                    RestAPIManager.getInstance().register(username, email, password, RegisterActivity.this);
+
+                    Intent profileIntent = new Intent(getApplicationContext(), ProfileCreateActivity.class);
+                    startActivity(profileIntent);
+                }
             }
+
         });
 
         txtLogin = findViewById(R.id.text_login);
@@ -49,9 +64,43 @@ public class RegisterActivity extends Activity implements RegisterCallBack {
                 finish();
             }
         });
-
     }
 
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String user = textUser.getText().toString();
+        String email = textMail.getText().toString();
+        String password = textPassword.getText().toString();
+        String repassword = textRePassword.getText().toString();
+
+        if (user.isEmpty()) {
+            textUser.setError(getText(R.string.error_user));
+            valid = false;
+        }
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            textMail.setError(getText(R.string.error_mail));
+            valid = false;
+        } else {
+            textPassword.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            textPassword.setError(getText(R.string.error_pass));
+            valid = false;
+        } else {
+            textPassword.setError(null);
+        }
+
+        if (repassword.isEmpty() || !repassword.equals(password)) {
+            textRePassword.setError(getText(R.string.error_repass));
+            valid = false;
+        }
+
+        return valid;
+    }
 
 
     @Override
